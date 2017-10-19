d7687076b4f89ce255e3808c2c6a7ca4449eb973
CaitlinRC <caitlinburns2308@gmail.com>
2017-10-19 10:33:24 +0100

    added list of relevant functions

--- game.py
+++ game.py
@@ -4,3 +4,10 @@ from map import rooms
 from items import *
 from player import *
 from gameparser import *
+
+# Functions Needed:
+# Move
+# Take
+# Drop
+# Use
+# Something to deal with Speech (??)
