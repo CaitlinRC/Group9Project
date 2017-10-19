b78c9f859abb15f685de0a689f789df87cffcf9a
CaitlinRC <caitlinburns2308@gmail.com>
2017-10-19 09:29:22 +0100

    Added item descriptions/names

---
+++ items.py
@@ -0,0 +1,58 @@
+item_whiskey = {
+    "id": "whiskey",
+
+    "name": "Bottle Of Whiskey",
+
+    "description":
+    """A bottle of very old whiskey that seems extremely flammable, perfect as fuel for your ship."""}
+
+
+item_trident = {
+    "id": "trident",
+
+    "name": "Poseidon's Trident",
+
+    "description":
+    """The grand weapon of the great god Poseidon and an item of great interest to his brother..."""}
+
+
+item_sword = {
+    "id": "sword",
+
+    "name": "Knight's Sword",
+
+    "description":
+    """A sharp sword, taken from the medieval period. It can cut through almost anything."""}
+
+
+item_armour = {
+    "id": "armour",
+
+    "name": "Suit Of Armour",
+
+    "description":
+    """A strong suit of armour, taken from the medieval period. It can cover your entire body, leaving no section of skin unexposed. Complete protection."""}
+
+
+item_gem = {
+    "id": "gem",
+
+    "name": "Mystical Gem",
+
+    "description": "The refraction capabilities of this gem could power your generator for months. Taken from the mummy's grasp."""}
+
+
+item_ore = {
+    "id": "ore",
+
+    "name": "Unknown Metal Ore",
+
+    "description": "This ore is made from a material that you do not recognise. Pieces of a potentially useful metal glisten in the centre of it, you just need to cut to it."""}
+
+
+item_keys = {
+    "id": "keys",
+
+    "name": "Spaceship Keys",
+
+    "description": "The keys to your ship, with your lovely Hello Kitty keychain. It's even got glitter on it!"""}
