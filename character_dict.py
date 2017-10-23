d04b109b2ab959da8eb117ed499b4e2f196afda5
Ahmed Bazina <32731620+AhmedB1@users.noreply.github.com>
2017-10-23 19:33:00 +0100

    updated

--- character_dict.py
+++ character_dict.py
@@ -1,11 +1,3 @@
-<<<<<<< HEAD
-character { repair_shop: "to fix your spaceship you must trvel through diffent dimentions to find the nessesary parts to fix your ship !",
-           cowboy: "hey i haven't seen your face in this part of town before ! I dont like the look of you, leave this bar before things get ugly !",
-           bar_tender: " wow I don't want to mess with you ! whatever you want on the house",
-           persiden: "to recieve my tridant you must first solve this riddle !, poor people have it , rich people need it and if you eat it you die",
-           gaurd: "to cross this threshhold you must have at least 2 lives left or the fire will burn you alive! if you manage to cross this boudery haydes will only give you what you want when you forst give him his brothers most precious item",
-           Haydes: "WELCOME TO THE UNDERWORLD. WHERE IS MY TRIDANT"
-=======
 character {
     cowboy: """Howdy! I haven\'t seen your face in this part of town before.
 I don\'t like the look of you, leave this bar before you get your ass kicked!""",
@@ -20,7 +12,6 @@ He has something of yours that fell from the sky, maybe if you give him his brot
 
     hades: """Welcome to my domain. I have something of yours and I am willing to give it to you.
 Only if, you give me what I want. Do not refuse me or there will be hell to pay."""
->>>>>>> 6f12d9417a7149a8f1d53c0a67ee4c1538e729d2
            }
 
 
