26347ff6bd4878c2eab529bb25c29a95fdaa0041
CaitlinRC <caitlinburns2308@gmail.com>
2017-10-23 21:36:39 +0100

    Adding interactions

--- game.py
+++ game.py
@@ -181,7 +181,7 @@ def execute_go(direction):
     """
     global current_room
     exits = current_room['exits']
-
+    
     if is_valid_exit(exits, direction):
         current_room = move(exits, direction)
 
--- interactions.py
+++ interactions.py
@@ -25,28 +25,39 @@ def mummy_interaction():
 
     print('As you step closer to the sarcophagus, a mummy rises from it and staggers towards you.\nIt\'s teeth are gnashing for a taste of your skin. You can see the gems you need glinting behind it.')
 
-    user_input = input('\nYou can: Attempt to TAKE the gem, GO SOUTH to the previous room or attempt to FIGHT: ')
+    while True:
+        
+        user_input = input('\nYou can: Attempt to TAKE the gem, GO SOUTH to the previous room or attempt to FIGHT: ')
 
-    filtered_input = normalise_input(user_input)
+        filtered_input = normalise_input(user_input)
 
-    if (filtered_input[0] == 'take') and (filtered_input[1] == 'gem'):
+        if (len(filtered_input) == 1) and (filtered_input[0] == 'fight'):
 
-        if item_armour["worn"] == True:
-
-            print('You grab the gem, the mummys teeth unable to harm you in your suit of armour.')
-
-        else:
-            print('The mummy\'s teeth sink into your skin! If only you had some protection... (-1 Life)')
+            print('You attempt to attack the mummy, flailing at it with your weapons. They have no effect.\nThe mummy clearly doesn\'t feel pain and continues to sink its teeth into your skin. (-1 Life)')
             lives -= 1
 
-    elif (filtered_input[0] == 'go') and (filtered_input[1] == 'south'):
+        elif (filtered_input[0] == 'take') and (filtered_input[1] == 'gem'):
 
-        execute_go(filtered_input[1])
+            if item_armour["worn"] == True:
 
-    elif filtered_input[0] == 'fight':
+                print('You grab the gem, the mummys teeth unable to harm you in your suit of armour.')
+                False
 
-        print('You attempt to attack the mummy, flailing at it with your weapons. They have no effect.\nThe mummy clearly doesn\'t feel pain and continues to sink its teeth into your skin. (-1 Life)')
-        lives -= 1
+            else:
+                print('The mummy\'s teeth sink into your skin! If only you had some protection... (-1 Life)')
+                lives -= 1
+
+        elif (filtered_input[0] == 'go') and (filtered_input[1] == 'south'):
+
+            execute_go("south")
+            False
+
+        else:
+            print('You cannot do that.')
+
+    
+
+        
         
     print(lives)
 
